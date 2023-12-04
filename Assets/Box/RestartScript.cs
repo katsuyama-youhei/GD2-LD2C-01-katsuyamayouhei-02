6e6744310a807c348898e6bea289278d21ed6be6
katsuyama-youhei <K022G0044@g.neec.ac.jp>
2023-12-04 13:08:58 +0900

    水に当たったのちリスタートできるように変更（ボタン入力

---
+++ Assets/Box/RestartScript.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class RestartScript : MonoBehaviour
+{
+    private GameObject player;
+    private MoveScript playerMove;
+
+    [SerializeField] private string loadScene;
+    [SerializeField] private Color fadeColor = Color.black;
+    [SerializeField] private float fadeSpeedMultiplier = 1.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        playerMove = player.GetComponent<MoveScript>();
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(!playerMove.isAlive) {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                Initiate.Fade(loadScene, fadeColor, fadeSpeedMultiplier);
+            }
+        }
+       
+    }
+
+   
+}
